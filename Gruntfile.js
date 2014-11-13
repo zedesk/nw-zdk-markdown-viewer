68e4bd48fccfd22b34f9bdf74333aa57ff864075
fabrice <fabrice.lecoz@zedesk.net>
2014-11-13 16:41:07 +0100

    Change version to 0.2.0-pre

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,23 +3,24 @@
 /* jshint node:true */
 
 module.exports = function(grunt) {
-    grunt.initConfig({
-        nodewebkit: {
-            options: {
-                version: 'latest',
-                buildDir:"./dist",
-                buildType:"versioned",
-                appVersion:"0.1.2",
-                // choose what platforms to compile for here
-                platforms:['osx', 'linux32', 'linux64', 'win'],
-                macIcns: './icns/MMD.icns',
-                macPlist:'./Info.plist',
-                winIco:'./icns/MMD.ico'
-            },
-            src: ['./src/**/*']
-        }
-    });
+	grunt.initConfig({
+		nodewebkit: {
+			options: {
+				version: 'latest',
+				buildDir:"./dist",
+				buildType:"versioned",
+				appVersion:"0.2.0-pre",
+				// choose what platforms to compile for here
+				// platforms:['osx', 'linux32', 'linux64', 'win'],
+				platforms:['osx', 'linux32', 'linux64'],
+				macIcns: './icns/MMD.icns',
+				macPlist:'./Info.plist',
+				winIco:'./icns/MMD.ico'
+			},
+			src: ['./src/**/*']
+		}
+	});
 
-    grunt.loadNpmTasks('grunt-node-webkit-builder');
-    grunt.registerTask('default', ['nodewebkit']);
+	grunt.loadNpmTasks('grunt-node-webkit-builder');
+	grunt.registerTask('default', ['nodewebkit']);
 };
